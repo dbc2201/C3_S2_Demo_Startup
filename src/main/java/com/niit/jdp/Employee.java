53fcc17ee91c942a53198d44d12500e33c0184cc
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-24 15:00:16 +0530

    created a parameterized constructor for the Employee type

--- src/main/java/com/niit/jdp/Employee.java
+++ src/main/java/com/niit/jdp/Employee.java
@@ -15,4 +15,13 @@ public class Employee {
 
     public Employee() {
     }
+
+    public Employee(String name, int code, int age, String dateOfBirth, String address, double salary) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.salary = salary;
+    }
 }
