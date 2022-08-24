e84bfad57ee0863abeb36ee7e6c9d24a1ec3b2cd
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-24 15:09:03 +0530

    calculated the annual salary for daniel in the main() method

--- src/main/java/com/niit/jdp/EmployeeImpl.java
+++ src/main/java/com/niit/jdp/EmployeeImpl.java
@@ -7,6 +7,8 @@ package com.niit.jdp;
 
 public class EmployeeImpl {
     public static void main(String[] args) {
-        Employee daniel = new Employee("Daniel", 130, 32, "12/12/1990", "124 Bridgewater Eville", 300.0);
+        Employee daniel = new Employee("Daniel", 130, 32, "12/12/1990", "124 Bridgewater Eville", 3000.0);
+        double annualSalary = daniel.calculateAnnualSalary();
+        System.out.println("annualSalary = $" + annualSalary);
     }
 }
